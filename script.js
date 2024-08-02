77dbbba49804461a7919af0e511cefeb4668cd94
nicoly <83140280+nicolyfxp@users.noreply.github.com>
2024-08-02 07:46:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector ('.caixaPrincipal');
+const caixaPergunta = document.querySelector ('.caixaPergunta');
+const caixaAlternativa = document.querySelector ('.caixaAlternativa');
+const caixaResultado = document.querySelector ('.caixaResultado');
+
+const perguntas = [
+ {
+enunciado: "Pergunta 1"
+alternativas: [ [
+"alternativa 1"
+"alternativa 2"
+]
+ }
+]
