76834adf194c7816b9dbba31ea7a05d377ba6f3f
nicoly <83140280+nicolyfxp@users.noreply.github.com>
2024-08-08 08:32:14 -0300

    .

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ const caixaResultado = document.querySelector ('.caixaResultado');
 
 const perguntas = [
  {
-enunciado: "Pergunta 1"
-alternativas: [ [
-"alternativa 1"
-"alternativa 2"
-]
+   enunciado: "Pergunta 1"
+   alternativas: [ [ "alternativa 1" , "alternativa 2"]
+ }
+ {
+   enunciado: "Pergunta 1"
+   alternativas: [ [ "alternativa 1" , "alternativa 2"]
+}
+{
+   enunciado: "Pergunta 1"
+   alternativas: [ [ "alternativa 1" , "alternativa 2"]
  }
 ]
+
+let atual =0;
+let PerguntaAtual;
+
+function mostraPergunta(){
+    
+}
